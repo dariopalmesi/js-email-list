f6eb3141ba5077600334dcfa2b89f97e27517e51
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-17 17:14:36 +0200

    upgrade end commit

--- main.js
+++ main.js
@@ -3,22 +3,38 @@
 //     const mails = response.data.response
 //     document.querySelector('h1').innerHTML =mails
 // })
-const mailsEl = document.getElementById('mails')
+// const mailsEl = document.getElementById('mails')
 
-for (let i= 0; i <= 10; i++) {
+// for (let i= 0; i <= 10; i++) {
+//     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//     .then(responce => {
+//         let mails = responce.data.responce
+//         console.log(mails);
+        
+//         // // let {email} = mails
+//         // let markup = `
+//         // <h1>${email}</h1>
+//         // `
+
+//         // mailsEl.innerHTML += markup
+    
+        
+//     })
+    
+// }
+
+const mailsEl = document.getElementById('mails')
+for (let i= 0; i < 10; i++) {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(responce => {
-        let mails = responce.data
+        let mails = responce.data.response
         console.log(mails);
         
-        let {email} = mails
+        
         let markup = `
-        <h1>${email}</h1>
+        <h1>${mails}</h1>
         `
-
         mailsEl.innerHTML += markup
-        console.log(mails);
-        
     })
     
 }
